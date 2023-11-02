c16680e9517e17d8d2a41a0b405e47a499dc4802
Artem Stepanenko <a.stsepanenka@astondevs.ru>
2023-11-02 12:45:53 +0300

    add tab button

---
+++ app/src/main/java/com/blogstour/app/ui/screen/home/model/UiTabButton.kt
@@ -0,0 +1,9 @@
+package com.blogstour.app.ui.screen.home.model
+
+data class UiTabButton(
+    val color: String,
+    val icon: String,
+    val title: String,
+    val type: String,
+    val url: String
+)
